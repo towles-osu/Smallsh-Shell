009ee6a592372cf46e2cfe1e78bc420214346c4f
towles-osu <towles@os1.engr.oregonstate.edu>
2021-11-01 16:56:10 -0700

    made files

---
+++ smallsh.c
@@ -0,0 +1,36 @@
+//Author: Stew Towle
+//Written for OSU's CS344 Operating Systems class, Assignment 3
+//Must be compiled with gcc compiler and --std=gnu99 option
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+//may need to add an include of <time.h>, but don't think I need it.
+
+//------------Program Description--------------
+
+
+
+
+
+
+
+
+
+
+//------main function------
+
+
+int main(int argc, char *argv[]){
+
+
+  return EXIT_SUCCESS;
+
+}
